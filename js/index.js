6068d9da4cce60ce9186ec2d37428966e827075c
FurukawaMamiko <aquamarine.mamiko@gmail.com>
2021-06-08 12:37:18 +0900

    データ計測

--- js/index.js
+++ js/index.js
@@ -5,4 +5,34 @@ const EMOTIONAL_LIST = [
     'おこる',
     '好き',
 ];
-const EMOTIONAL_ID = 
+const EMOTIONAL_ID = Math.floor(Math.random() * EMOTIONAL_LIST.length);
+const EMOTIONAL_LABEL = EMOTIONAL_LIST[EMOTIONAL_ID];
+
+const initilize = function() {
+    const questionLabel = document.querySelector('#js-question-label');
+    const buttonList = document.querySelectorAll('.js-button');
+
+    questionLabel.textContent = EMOTIONAL_LABEL;
+    
+    buttonList.forEach(function(button, i) {
+        button.addEventListener('click', function (e) {
+            gtag("event", "select_content", {
+                // 感情名をcontent_typeとする
+                content_type: `select_shape_emotion_${EMOTIONAL_LABEL}`,
+                // ボタンのインデックス（何番目のボタンか）の数値を選択したIDとする
+                item_id: i,
+            });
+        });
+    });
+};
+
+if (document.readyState !== 'loading') {
+    // HTML が最後まで読み込まれていれば初期化する
+    initilize();
+} else {
+    // HTML が最後まで読み込まれていなければ、
+    // 読み込みの終了(DOMContentLoaded)を検知して初期化する
+    document.addEventListener('DOMContentLoaded', function () {
+        initilize();
+    });
+}
