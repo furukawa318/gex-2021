cbf66baa7de0ae8e42351dd7fa7e71e322ee3fb2
FurukawaMamiko <aquamarine.mamiko@gmail.com>
2021-06-08 14:02:27 +0900

    no message

--- js/sketch1.js
+++ js/sketch1.js
@@ -1,11 +1,13 @@
-function setup() {
-    createCanvas(w, h);
-    // background('rgba(255,255,0, 0.25)');
-    
-}
+const pane = new Tweakpane.Pane();
+const pane = new Pane();
 
-function draw() {
-    
+const PARAMS = {
+    factor: 123,
+    title: 'hello',
+    color: '#0f0',
+  };
+  
+  pane.addInput(PARAMS, 'factor');
+  pane.addInput(PARAMS, 'title');
+  pane.addInput(PARAMS, 'color');
 
-
-}
