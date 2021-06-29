5b9afb1327bfd8bba076e531afa68e46c3550df9
FurukawaMamiko <aquamarine.mamiko@gmail.com>
2021-06-29 18:34:18 +0900

    2021/06/29進捗

--- js/tweakpane.js
+++ js/tweakpane.js
@@ -1,30 +1,32 @@
-function setup() {
-  createCanvas(800, 500);
-  fill(255, 204, 0);
-  rect(20, 20, 100, 100);
-}
-
 
 const box_el = document.getElementById("box");
 console.log(document.getElementById("box"));
 
+
 const PARAMS = {
   factor: 123,
-  color: '#000',
+  background: {r: 160, g: 56, b: 56},
   opacity: 0.5,
 };
 
 const pane = new Tweakpane.Pane(); //パネルの初期化
 
 pane.addInput(PARAMS, 'factor');
-pane.addInput(PARAMS, 'color');
+pane.addInput(PARAMS, 'background');
 pane.addInput(PARAMS, 'opacity', {
   min: 0,
   max: 1,
 }); //パラメーターをパネルにセットする
 
 pane.on('change', (val) => {
-  console.log(PARAMS.opacity);
+  // console.log(PARAMS.opacity);
   box_el.style.opacity = PARAMS.opacity;
 });
+
+pane.on('change', () => {
+  box_el.style.background = `rgb(${PARAMS.background.r}, ${PARAMS.background.g}, ${PARAMS.background.b})`;
+  // console.log(`rgb(${PARAMS.background.r}, ${PARAMS.background.g}, ${PARAMS.background.b})`);
+});
+
+
 
